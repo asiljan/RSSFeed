73123171f8a2387215c06a6ca68614834baca807
Alen Siljan <alen.siljan@gmail.com>
2017-03-28 19:11:36 +0200

    Build DI in application class

--- app/src/main/java/com/android/rssfeed/RSSFeedApplication.java
+++ app/src/main/java/com/android/rssfeed/RSSFeedApplication.java
@@ -2,6 +2,10 @@ package com.android.rssfeed;
 
 import android.app.Application;
 
+import com.android.rssfeed.di.components.DaggerRssFeedAppComponent;
+import com.android.rssfeed.di.components.RssFeedAppComponent;
+import com.android.rssfeed.di.modules.AppModule;
+
 /**
  * Created by Alen Siljan on 28.3.2017..
  * alen.siljan@gmail.com
@@ -10,12 +14,17 @@ import android.app.Application;
 public class RSSFeedApplication extends Application {
 
     private static RSSFeedApplication instance = null;
+    private RssFeedAppComponent mAppComponent;
 
     @Override
     public void onCreate() {
         super.onCreate();
 
         setInstance(this);
+
+        mAppComponent = DaggerRssFeedAppComponent.builder()
+                .appModule(new AppModule(this))
+                .build();
     }
 
     public static void setInstance(RSSFeedApplication instance) {
@@ -24,5 +33,9 @@ public class RSSFeedApplication extends Application {
 
     public static RSSFeedApplication getInstance() {
         return instance;
+    }
+
+    public RssFeedAppComponent getAppComponent() {
+        return mAppComponent;
     }
 }
