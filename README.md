1f5501b83dff326d707345d8c976df578345be17
Alen Siljan <alen.siljan@gmail.com>
2017-03-28 18:42:07 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# RSS Feed Android Application #
+
+Simple android application which reads list of feeds and displays in list
+
+Developed by Alen Siljan in Zagreb, Croatia, using Java.
+
+### Owner ###
+
+Alen Siljan - alen.siljan@gmail.com
---
+++ app/src/main/java/com/android/rssfeed/RSSFeedActivity.java
@@ -0,0 +1,13 @@
+package com.android.rssfeed;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RSSFeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rssfeed);
+    }
+}
