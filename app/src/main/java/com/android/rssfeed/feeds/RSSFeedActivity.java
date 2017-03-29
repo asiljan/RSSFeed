a379ae76ef853be12232ac40782f86be90bda70f
Alen Siljan <alen.siljan@gmail.com>
2017-03-29 19:02:32 +0200

    Update feeds activity

--- app/src/main/java/com/android/rssfeed/feeds/RSSFeedActivity.java
+++ app/src/main/java/com/android/rssfeed/feeds/RSSFeedActivity.java
@@ -1,15 +1,43 @@
 package com.android.rssfeed.feeds;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.android.rssfeed.R;
+import com.android.rssfeed.feeds.callbacks.IRssFeedCallback;
 
-public class RSSFeedActivity extends AppCompatActivity {
+public class RSSFeedActivity extends AppCompatActivity implements IRssFeedCallback {
+
+    public static final int FRAGMENT_HOLDER = R.id.fragment_holder;
+
+    private RssFeedFragmentManager mFragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rssfeed);
+
+        mFragmentManager = new RssFeedFragmentManager(getSupportFragmentManager());
+
+        mFragmentManager.showRssFeedListFragment();
+    }
+
+    @Override
+    public void onRssFeedClicked(String url) {
+        //// TODO: 28.3.2017.
+    }
+
+    @Override
+    public void onBackRequest() {
+        onBackPressed();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mFragmentManager.backStackGreatherThanOne()) {
+            super.onBackPressed();
+        } else {
+            this.finish();
+        }
     }
 }
