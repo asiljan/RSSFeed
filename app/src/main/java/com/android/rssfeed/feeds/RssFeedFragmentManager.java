f5775dd909e3e771f937149c47894e3946de4e9a
Alen Siljan <alen.siljan@gmail.com>
2017-03-29 19:03:47 +0200

    Add custom fragmentManager class for fragment management

---
+++ app/src/main/java/com/android/rssfeed/feeds/RssFeedFragmentManager.java
@@ -0,0 +1,41 @@
+package com.android.rssfeed.feeds;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
+import com.android.rssfeed.feeds.fragments.RssFeedDetailsFragment;
+import com.android.rssfeed.feeds.fragments.RssFeedsFragment;
+
+/**
+ * Created by Alen Siljan on 28.3.2017..
+ * alen.siljan@gmail.com
+ */
+
+public class RssFeedFragmentManager {
+
+    private FragmentManager mFragmentManager;
+
+    public RssFeedFragmentManager(FragmentManager fragmentManager) {
+        this.mFragmentManager = fragmentManager;
+    }
+
+    public void showRssFeedListFragment() {
+        FragmentTransaction fTransaction = mFragmentManager.beginTransaction();
+        RssFeedsFragment fragment = new RssFeedsFragment();
+        fTransaction.replace(RSSFeedActivity.FRAGMENT_HOLDER, fragment, RssFeedsFragment.TAG);
+        fTransaction.addToBackStack(null);
+        fTransaction.commit();
+    }
+
+    public void showRssFeedDetailsFragment(String url) {
+        FragmentTransaction fTransaction = mFragmentManager.beginTransaction();
+        RssFeedDetailsFragment fragment = RssFeedDetailsFragment.newInstance(url);
+        fTransaction.replace(RSSFeedActivity.FRAGMENT_HOLDER, fragment, RssFeedDetailsFragment.TAG);
+        fTransaction.addToBackStack(null);
+        fTransaction.commit();
+    }
+
+    public boolean backStackGreatherThanOne() {
+        return mFragmentManager.getBackStackEntryCount() > 1;
+    }
+}
