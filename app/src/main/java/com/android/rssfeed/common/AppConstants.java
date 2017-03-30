32b711f47a70ed8bfef8e21c0300e2b614388cc7
Alen Siljan <alen.siljan@gmail.com>
2017-03-30 20:12:12 +0200

    Add AppConstants class

---
+++ app/src/main/java/com/android/rssfeed/common/AppConstants.java
@@ -0,0 +1,10 @@
+package com.android.rssfeed.common;
+
+/**
+ * Created by Alen Siljan on 30.3.2017..
+ * alen.siljan@gmail.com
+ */
+
+public class AppConstants {
+    public static final String FEEDS_DB_CHILD = "feeds";
+}
