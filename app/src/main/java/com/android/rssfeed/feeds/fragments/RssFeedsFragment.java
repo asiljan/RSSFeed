e53ba4346b24f7ba4287fd3f2616737307bef233
Alen Siljan <alen.siljan@gmail.com>
2017-03-30 20:11:53 +0200

    Update rssFeed fragment class

--- app/src/main/java/com/android/rssfeed/feeds/fragments/RssFeedsFragment.java
+++ app/src/main/java/com/android/rssfeed/feeds/fragments/RssFeedsFragment.java
@@ -2,8 +2,12 @@ package com.android.rssfeed.feeds.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -12,12 +16,15 @@ import android.widget.Spinner;
 
 import com.android.rssfeed.R;
 import com.android.rssfeed.common.BaseFragment;
-import com.android.rssfeed.common.helpers.LogHelper;
+import com.android.rssfeed.data.models.FeedItemModel;
 import com.android.rssfeed.di.components.RssFeedAppComponent;
 import com.android.rssfeed.di.modules.RssFeedModule;
+import com.android.rssfeed.feeds.adapters.RssFeedAdapter;
 import com.android.rssfeed.mvp.presenters.IRssFeedPresenter;
 import com.android.rssfeed.mvp.views.RssFeedView;
 import com.android.rssfeed.util.ResourceUtil;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -39,6 +46,8 @@ public class RssFeedsFragment extends BaseFragment implements RssFeedView, Adapt
 
     @Inject
     IRssFeedPresenter mPresenter;
+    @Inject
+    RssFeedAdapter mFeedItemsAdapter;
 
     @Override
     protected void injectDependencies(RssFeedAppComponent appComponent) {
@@ -58,7 +67,7 @@ public class RssFeedsFragment extends BaseFragment implements RssFeedView, Adapt
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState) {
         mView = inflater.inflate(R.layout.fragment_rssfeed, container, false);
 
         setHasOptionsMenu(true);
@@ -66,12 +75,34 @@ public class RssFeedsFragment extends BaseFragment implements RssFeedView, Adapt
         bindViews();
 
         setToolbar(ResourceUtil.getStringById(getContext(), R.string.rssFeedToolbarTitle), false);
+        initAdapter();
 
         return mView;
     }
 
-    private void initAdapter() {
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.clear();
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.main_rss_feed_menu, menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.rssFeedAddNew:
+                mCallback.onAddNewFeedReuqest();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void initAdapter() {
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+        mRssFeedList.setHasFixedSize(true);
+        mRssFeedList.setLayoutManager(layoutManager);
+        mRssFeedList.setAdapter(mFeedItemsAdapter);
     }
 
     @Override
@@ -89,6 +120,16 @@ public class RssFeedsFragment extends BaseFragment implements RssFeedView, Adapt
     }
 
     @Override
+    public void onFeedParsed(List<FeedItemModel> feedItems) {
+        mFeedItemsAdapter.updateFeedItems(feedItems);
+    }
+
+    @Override
+    public void onFeedParseFail() {
+        //// TODO: 29.3.2017.
+    }
+
+    @Override
     public void onShowLoadingBar() {
         showLoadingBar();
     }
@@ -101,7 +142,7 @@ public class RssFeedsFragment extends BaseFragment implements RssFeedView, Adapt
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         if (position > 0) {
-            LogHelper.printLogMessage("INFO selected item: " + parent.getItemAtPosition(position));
+            mPresenter.onFeedSelected(position);
         }
     }
 
